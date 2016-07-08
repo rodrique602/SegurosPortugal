232755e3f657032668abc68ac44405e1f1585991
rodriquez602 <rodriquez602@msn.com>
2016-07-07 21:08:16 -0700

    se agregaron modelos controladores y vistas  de client y automobilePolicy y se hizo las relaciones

---
+++ Portugal Insurance - PayPal/Controllers/AutomobilePolicyController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Portugal_Insurance___PayPal.Models;
+using Portugal_Insurance___PayPal.DAL;
+
+namespace Portugal_Insurance___PayPal.Controllers
+{
+    public class AutomobilePolicyController : Controller
+    {
+        private Portugal_Insurance___PayPalContextDB db = new Portugal_Insurance___PayPalContextDB();
+
+        // GET: /AutomobilePolicy/
+        public ActionResult Index()
+        {
+            var automobilepolicies = db.AutomobilePolicies.Include(a => a.client);
+            return View(automobilepolicies.ToList());
+        }
+
+        // GET: /AutomobilePolicy/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AutomobilePolicy automobilepolicy = db.AutomobilePolicies.Find(id);
+            if (automobilepolicy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(automobilepolicy);
+        }
+
+        // GET: /AutomobilePolicy/Create
+        public ActionResult Create()
+        {
+            ViewBag.clientID = new SelectList(db.Clients, "clientID", "fullName");
+            return View();
+        }
+
+        // POST: /AutomobilePolicy/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="automobilePolicyID,vehicleValue,vehicleVin,carYear,carMake,carModel,policyFolio,policySold,policySoldDate,policyStartingDate,policyEndingDate,clientID")] AutomobilePolicy automobilepolicy)
+        {
+            if (ModelState.IsValid)
+            {
+                db.AutomobilePolicies.Add(automobilepolicy);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.clientID = new SelectList(db.Clients, "clientID", "fullName", automobilepolicy.clientID);
+            return View(automobilepolicy);
+        }
+
+        // GET: /AutomobilePolicy/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AutomobilePolicy automobilepolicy = db.AutomobilePolicies.Find(id);
+            if (automobilepolicy == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.clientID = new SelectList(db.Clients, "clientID", "fullName", automobilepolicy.clientID);
+            return View(automobilepolicy);
+        }
+
+        // POST: /AutomobilePolicy/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="automobilePolicyID,vehicleValue,vehicleVin,carYear,carMake,carModel,policyFolio,policySold,policySoldDate,policyStartingDate,policyEndingDate,clientID")] AutomobilePolicy automobilepolicy)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(automobilepolicy).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.clientID = new SelectList(db.Clients, "clientID", "fullName", automobilepolicy.clientID);
+            return View(automobilepolicy);
+        }
+
+        // GET: /AutomobilePolicy/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AutomobilePolicy automobilepolicy = db.AutomobilePolicies.Find(id);
+            if (automobilepolicy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(automobilepolicy);
+        }
+
+        // POST: /AutomobilePolicy/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            AutomobilePolicy automobilepolicy = db.AutomobilePolicies.Find(id);
+            db.AutomobilePolicies.Remove(automobilepolicy);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Portugal Insurance - PayPal/Models/Client.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Portugal_Insurance___PayPal.Models
+{
+    public class Client
+    {
+
+    [Key]
+    [Display(Name="Client ID")]
+     public int clientID { get; set; }
+
+    [Display(Name = "Full Name")]
+    public String fullName { get; set; }
+
+    [Display(Name = "address Line1")]
+    public String addressLine1 { get; set; }
+    [Display(Name = "address Line1")]
+    public String addressLine2 { get; set; }
+
+    [Display(Name = "City")]
+    public String city { get; set; }
+
+    [Display(Name = "State")]
+    public String state { get; set; }
+
+    [Display(Name = "Zip Code")]
+    public String zipCode { get; set; }
+
+    [Display(Name = "Country")]
+    public String country { get; set; }
+
+    [Display(Name = "Phone Number")]
+    public String phoneNumber { get; set; }
+
+    [Display(Name = "Email Address")]
+    public String emailAddress { get; set; }
+
+    [Display(Name = "Lincense Number 1")]
+    public String licenseNumber1 { get; set; }
+
+    [Display(Name = "Lincense Number 2")]
+    public String licenseNumber2 { get; set; }
+
+
+        //Un cliente puede tener muchas polizas
+    public virtual ICollection<AutomobilePolicy> AutomobilePolicies { get; set; }
+
+    }
+}
