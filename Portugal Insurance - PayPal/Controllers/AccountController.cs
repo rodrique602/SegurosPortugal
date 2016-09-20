6daef400684718089d6f2a5a1fcebed5dfe4695f
rodriquez602 <rodriquez602@msn.com>
2016-09-20 09:25:21 -0700

    Changes in the Home Index and in the Precios Detals Views.

--- Portugal Insurance - PayPal/Controllers/AccountController.cs
+++ Portugal Insurance - PayPal/Controllers/AccountController.cs
@@ -79,14 +79,15 @@ namespace Portugal_Insurance___PayPal.Controllers
             if (ModelState.IsValid)
             {
                 var user = new ApplicationUser() { UserName = model.UserName, fullName = model.fullName, addressLine1 = model.addressLine1,
-                addressLine2 = model.addressLine2, city = model.city, country = model.country, emailAddress = model.emailAddress,
+                addressLine2 = model.addressLine2, city = model.city, country = model.country, emailAddress =  model.emailAddress,
                  licenseNumber1 = model.licenseNumber1, licenseNumber2 = model.licenseNumber2, phoneNumber = model.phoneNumber,
                  state = model.state, zipCode = model.zipCode};
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    await this.UserManager.AddToRoleAsync(user.Id,AccountRolesNames.CLIENT);
                     await SignInAsync(user, isPersistent: false);
-                    return RedirectToAction("Index", "Home");
+                    return Redirect(Request.UrlReferrer.ToString());
                 }
                 else
                 {
